864a700848e70bc3ca6120faba4fd4db7b0ba25b
Michael Lybecait <michael.lybecait@tu-dortmund.de>
2014-04-25 10:38:30 +0200

    use the new URIHandler to save ecore files with nsuri

--- de.jabc.cinco.meta.core.mgl/src/de/jabc/cinco/meta/core/mgl/generator/GenModelCreator.java
+++ de.jabc.cinco.meta.core.mgl/src/de/jabc/cinco/meta/core/mgl/generator/GenModelCreator.java
@@ -1,20 +1,16 @@
 package de.jabc.cinco.meta.core.mgl.generator;
 
-import java.util.Collections;
+import java.util.ArrayList;
 
-import  org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.emf.codegen.ecore.genmodel.GenJDKLevel;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModelFactory;
 import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
 import org.eclipse.emf.codegen.ecore.genmodel.GenRuntimeVersion;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.importer.ecore.EcoreImporter;
 
 public class GenModelCreator {	
 	public static GenModel createGenModel(IPath ecorePath,EPackage ePackage,String projectName,IPath projectPath){
@@ -24,14 +20,11 @@ public class GenModelCreator {
                 Resource.Factory.Registry.INSTANCE.getFactory(genModelURI).createResource(genModelURI); 
 		GenModel genModel = GenModelFactory.eINSTANCE.createGenModel(); 
 		genModelResource.getContents().add(genModel);
-        ResourceSet resourceSet = new ResourceSetImpl(); 
-        resourceSet.getResources().add(genModelResource);
-        System.out.println(projectPath);
         genModel.setModelDirectory("/"+projectPath.append("src-gen").toPortableString());
-        genModel.getForeignModel().add("platform:/resource/"+projectPath.append("src-gen/model").addTrailingSeparator().toPortableString()+ecorePath.lastSegment());
-        //genModel.getForeignModel().add("platform:/plugin/de.jabc.cinco.meta.core.mgl/model/GraphModel.ecore");
         
-        genModel.initialize(Collections.singleton(ePackage));
+        ArrayList<EPackage> ePackageList = new ArrayList<>();
+        ePackageList.add(ePackage);
+        genModel.initialize(ePackageList);
         GenPackage genPackage = (GenPackage)genModel.getGenPackages().get(0);
         genModel.setModelName(genModelURI.trimFileExtension().lastSegment());
         String s = ePackage.getNsPrefix();
@@ -44,9 +37,10 @@ public class GenModelCreator {
         genModel.setEditorPluginID(projectName+".editor");
         genModel.setTestsPluginID(projectName+".tests");
         genModel.setCanGenerate(true);
-        //genModel.getUsedGenPackages().addAll(genModel.computeMissingUsedGenPackages());
+
 
         
 		return genModel;
 	}
+
 }
