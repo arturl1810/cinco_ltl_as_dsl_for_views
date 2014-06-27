a3dd0ab66b501e63817983dd1eeefd7aa32079a3
Dawid Kopetzki <dawid.kopetzki@tu-dortmund.de>
2014-06-27 12:18:59 +0200

    Custom feature generation almost finished. TODO: Avoid overriding during regeneration step

--- de.jabc.cinco.meta.core.ge.style.model/src/de/jabc/cinco/meta/core/ge/style/model/customfeature/CincoCustomFeature.java
+++ de.jabc.cinco.meta.core.ge.style.model/src/de/jabc/cinco/meta/core/ge/style/model/customfeature/CincoCustomFeature.java
@@ -1,10 +1,13 @@
 package de.jabc.cinco.meta.core.ge.style.model.customfeature;
 
+import graphmodel.ModelElement;
+
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.context.ICustomContext;
 import org.eclipse.graphiti.features.custom.AbstractCustomFeature;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 
-public class CincoCustomFeature<T> extends AbstractCustomFeature {
+public abstract class CincoCustomFeature<T extends ModelElement> extends AbstractCustomFeature {
 
 	public CincoCustomFeature(IFeatureProvider fp) {
 		super(fp);
@@ -13,14 +16,27 @@ public class CincoCustomFeature<T> extends AbstractCustomFeature {
 
 	@Override
 	public boolean canExecute(ICustomContext context) {
-		// TODO Auto-generated method stub
-		return super.canExecute(context);
+		PictogramElement[] pes = context.getPictogramElements();
+		if (pes.length > 1)
+			return false;
+		if (pes.length == 0)
+			return false;
+		Object o = getBusinessObjectForPictogramElement(pes[0]);
+		T modelElement = null;
+		if (o instanceof ModelElement)
+			modelElement = (T) o;
+		return canExecute(modelElement); 
 	}
 	
 	@Override
 	public void execute(ICustomContext context) {
-		// TODO Auto-generated method stub
-		
+		if (canExecute(context)) {
+			T modelElement = (T) getBusinessObjectForPictogramElement(context.getPictogramElements()[0]);
+			execute(modelElement);
+		}
 	}
+	
+	public abstract boolean canExecute(T modelElement);
+	public abstract void execute(T modelElement);
 
 }
