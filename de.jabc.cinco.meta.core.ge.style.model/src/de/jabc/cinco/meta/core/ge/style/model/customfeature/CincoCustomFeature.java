ed75995f014b942491444e0fc4bf0098e83ef721
Dawid Kopetzki <dawid.kopetzki@tu-dortmund.de>
2014-06-27 10:18:45 +0200

    Added custom feature annotation and generator.

---
+++ de.jabc.cinco.meta.core.ge.style.model/src/de/jabc/cinco/meta/core/ge/style/model/customfeature/CincoCustomFeature.java
@@ -0,0 +1,26 @@
+package de.jabc.cinco.meta.core.ge.style.model.customfeature;
+
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.ICustomContext;
+import org.eclipse.graphiti.features.custom.AbstractCustomFeature;
+
+public class CincoCustomFeature<T> extends AbstractCustomFeature {
+
+	public CincoCustomFeature(IFeatureProvider fp) {
+		super(fp);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean canExecute(ICustomContext context) {
+		// TODO Auto-generated method stub
+		return super.canExecute(context);
+	}
+	
+	@Override
+	public void execute(ICustomContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
