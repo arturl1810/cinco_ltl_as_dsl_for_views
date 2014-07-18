751e60dd36f0334b1e5a649d5a138ae215808003
Dawid Kopetzki <dawid.kopetzki@tu-dortmund.de>
2014-07-18 10:10:36 +0200

    Bugfix: ClassCastException in update feature. Added connection constant in StyleAppearanceProvider

--- de.jabc.cinco.meta.core.ge.style.model/src/de/jabc/cinco/meta/core/ge/style/model/appearance/StyleAppearanceProvider.java
+++ de.jabc.cinco.meta.core.ge.style.model/src/de/jabc/cinco/meta/core/ge/style/model/appearance/StyleAppearanceProvider.java
@@ -3,5 +3,8 @@ package de.jabc.cinco.meta.core.ge.style.model.appearance;
 import style.Appearance;
 
 public interface StyleAppearanceProvider<T> {
+	
+	final String CONNECTION = "connection";
+	
 	public Appearance getAppearance(T object, String element);
 }
