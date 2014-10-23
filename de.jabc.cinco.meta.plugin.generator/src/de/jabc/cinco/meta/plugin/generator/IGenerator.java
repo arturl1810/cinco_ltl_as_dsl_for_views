e95695f00970c2b7be50e13a1b55da1558e93d94
Michael Lybecait <michael.lybecait@tu-dortmund.de>
2014-10-23 14:52:43 +0200

    added javadoc

--- de.jabc.cinco.meta.plugin.generator/src/de/jabc/cinco/meta/plugin/generator/IGenerator.java
+++ de.jabc.cinco.meta.plugin.generator/src/de/jabc/cinco/meta/plugin/generator/IGenerator.java
@@ -2,8 +2,14 @@ package de.jabc.cinco.meta.plugin.generator;
 
 import graphmodel.GraphModel;
 
-import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
 
-public interface IGenerator {
-	public void generate(GraphModel graphModel,IFile outlet);
+public interface IGenerator{
+	/**
+	 *
+	 * @param graphModel : Graphmodel which is to be generated.
+	 * @param outlet : Path to given outlet. When generate is called, existance of path is  guaranteed
+	 * If Exception occurs, RuntimeException may be thrown
+	 */ 
+	public void generate(GraphModel graphModel,IPath outlet);
 }
