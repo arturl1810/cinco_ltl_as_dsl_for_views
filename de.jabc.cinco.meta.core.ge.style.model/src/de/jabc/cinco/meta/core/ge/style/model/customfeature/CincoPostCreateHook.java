8269aee38da2fb36ca26f605d9dd2b479e97f056
Dawid Kopetzki <dawid.kopetzki@tu-dortmund.de>
2015-01-19 11:37:30 +0100

    Added the postCreateHooks.

--- de.jabc.cinco.meta.core.ge.style.model/src/de/jabc/cinco/meta/core/ge/style/model/customfeature/CincoPostCreateHook.java
+++ de.jabc.cinco.meta.core.ge.style.model/src/de/jabc/cinco/meta/core/ge/style/model/customfeature/CincoPostCreateHook.java
@@ -1,9 +1,26 @@
 package de.jabc.cinco.meta.core.ge.style.model.customfeature;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.graphiti.mm.pictograms.Diagram;
 
 abstract public class CincoPostCreateHook <T extends EObject> {
 
+	private Diagram diagram;
+	
 	abstract public void postCreate(T object);
+	
+	public void postCreate(T object, Diagram d) {
+		this.diagram = d;
+		postCreate(object);
+	}
+
+	public Diagram getDiagram() {
+		return diagram;
+	}
+
+	public void setDiagram(Diagram diagram) {
+		this.diagram = diagram;
+	}
+	
 	
 }
