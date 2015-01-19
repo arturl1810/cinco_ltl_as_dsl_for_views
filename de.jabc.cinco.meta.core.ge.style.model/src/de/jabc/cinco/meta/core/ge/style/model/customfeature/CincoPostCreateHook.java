8d7c30953462949c0cb498c5cdba61877cc47adf
Dawid Kopetzki <dawid.kopetzki@tu-dortmund.de>
2015-01-19 11:58:15 +0100

    Added diagram attribute to postCreateHook class.

--- de.jabc.cinco.meta.core.ge.style.model/src/de/jabc/cinco/meta/core/ge/style/model/customfeature/CincoPostCreateHook.java
+++ de.jabc.cinco.meta.core.ge.style.model/src/de/jabc/cinco/meta/core/ge/style/model/customfeature/CincoPostCreateHook.java
@@ -1,7 +1,13 @@
 package de.jabc.cinco.meta.core.ge.style.model.customfeature;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.IUpdateFeature;
+import org.eclipse.graphiti.features.context.impl.UpdateContext;
 import org.eclipse.graphiti.mm.pictograms.Diagram;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.eclipse.graphiti.services.Graphiti;
+import org.eclipse.graphiti.ui.services.GraphitiUi;
 
 abstract public class CincoPostCreateHook <T extends EObject> {
 
@@ -9,9 +15,19 @@ abstract public class CincoPostCreateHook <T extends EObject> {
 	
 	abstract public void postCreate(T object);
 	
-	public void postCreate(T object, Diagram d) {
+	public void postCreateAndUpdate(T object, Diagram d) {
 		this.diagram = d;
 		postCreate(object);
+		update(object);
+	}
+	
+	private void update(T object) {
+		UpdateContext uc = new UpdateContext(
+				(PictogramElement) Graphiti.getLinkService().getPictogramElements(getDiagram(), object).get(0));
+		IFeatureProvider provider = GraphitiUi.getExtensionManager().createFeatureProvider(getDiagram());
+		IUpdateFeature uf = provider.getUpdateFeature(uc);
+		if (uf != null && uf.canUpdate(uc))
+			uf.update(uc);
 	}
 
 	public Diagram getDiagram() {
