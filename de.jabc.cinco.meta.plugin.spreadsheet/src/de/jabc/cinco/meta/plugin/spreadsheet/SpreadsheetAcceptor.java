c7f20bc16a1556e0a494cfcf289d646bb9de34d0
philip zweihoff <philip.zweihoff@tu-dortmund.de>
2015-02-25 18:04:01 +0100

    Feature: Acceptor for file choosing on autocomplete

---
+++ de.jabc.cinco.meta.plugin.spreadsheet/src/de/jabc/cinco/meta/plugin/spreadsheet/SpreadsheetAcceptor.java
@@ -0,0 +1,46 @@
+package de.jabc.cinco.meta.plugin.spreadsheet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mgl.Annotation;
+
+import org.eclipse.xtext.ui.editor.contentassist.ConfigurableCompletionProposal.IReplacementTextApplier;
+
+import de.jabc.cinco.meta.core.pluginregistry.proposalprovider.IMetaPluginAcceptor;
+import de.jabc.cinco.meta.core.utils.xtext.ChooseFileTextApplier;
+
+public class SpreadsheetAcceptor implements IMetaPluginAcceptor {
+	
+	public SpreadsheetAcceptor() {
+	}
+
+	@Override
+	public List<String> getAcceptedStrings(Annotation annotation) {
+		ArrayList<String> aList = new ArrayList<>();
+		if(annotation.getName().equals("spreadsheet")){
+			if(annotation.getValue().size()==1) {
+				aList.add("\"single\"");
+				aList.add("\"multiple\"");				
+			}
+		}
+		if(annotation.getName().equals("resulting")){
+			if(annotation.getValue().size()==1) {
+				aList.add("Choose file...");
+			}
+		}
+		
+		return aList;
+		
+	}
+
+	@Override
+	public IReplacementTextApplier getTextApplier(Annotation annotation) {
+		if(annotation.getName().equals("resulting") && annotation.getValue().size()==1) {
+			return new ChooseFileTextApplier(annotation);
+
+		}
+		return null;
+	}
+
+}
