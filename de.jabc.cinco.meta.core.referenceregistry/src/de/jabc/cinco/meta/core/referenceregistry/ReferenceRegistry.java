17597632ac9cc03cf08f5ced8bd3bdbeccdd6225
Dawid Kopetzki <dawid.kopetzki@tu-dortmund.de>
2015-11-20 09:35:19 +0100

    Backup refreg.

--- de.jabc.cinco.meta.core.referenceregistry/src/de/jabc/cinco/meta/core/referenceregistry/ReferenceRegistry.java
+++ de.jabc.cinco.meta.core.referenceregistry/src/de/jabc/cinco/meta/core/referenceregistry/ReferenceRegistry.java
@@ -5,12 +5,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Properties;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
@@ -20,9 +17,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
-public class ReferenceRegistry implements Serializable{
-
-	private static final long serialVersionUID = 4355837301853271006L;
+public class ReferenceRegistry{
 
 	private static ReferenceRegistry instance;
 	private HashMap<Integer, String> map;
@@ -79,21 +74,16 @@ public class ReferenceRegistry implements Serializable{
 		File file = getFile();
 		if (file != null && file.exists()) {
 			try {
-				ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
-				Object o = ois.readObject();
-				count = ois.readInt();
-				ois.close();
-				if (o instanceof Map) {
-					map =  (HashMap<Integer, String>) o;
-					print();
-					return true;
+				FileInputStream fis = new FileInputStream(file);
+				Properties props = new Properties();
+				props.loadFromXML(fis);
+				for (Entry<Object, Object> e : props.entrySet()) {
+					map.put((Integer) e.getKey(), (String)e.getValue());
 				}
+				fis.close();
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -106,11 +96,12 @@ public class ReferenceRegistry implements Serializable{
 				if (!file.exists()) {
 					file.createNewFile();
 				}
-					ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
-					oos.writeObject(map);
-					oos.writeInt(count);
-					oos.close();
-					return true;
+				FileOutputStream fos = new FileOutputStream(file);
+				Properties props = new Properties();
+				props.putAll(map);
+				props.storeToXML(fos, "The reference registry");
+				fos.close();
+				return true;
 			} 
 			catch (IOException e) {
 				e.printStackTrace();
@@ -148,3 +139,149 @@ public class ReferenceRegistry implements Serializable{
 	}
 
 }
+
+
+/*package de.jabc.cinco.meta.core.referenceregistry;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+
+public class ReferenceRegistry{
+
+	private static ReferenceRegistry instance;
+	private HashMap<String, String> map;
+	
+	private int count = 0;
+	
+	private static final String REF_REG_FILE = "refReg.rrg";
+	
+	private ReferenceRegistry() {
+		map = new HashMap<String, String>();
+	}
+	
+	public static ReferenceRegistry getInstance() {
+		if (instance == null)
+			instance = new ReferenceRegistry();
+		return instance;
+	}
+	
+	public void addElement(EObject bo) {
+		String id = EcoreUtil.getID(bo);
+		if (id == null || id.isEmpty())
+			MessageDialog.openError(
+					Display.getCurrent().getActiveShell(), 
+					"Error adding Library Component", 
+					"The object " + bo + " has no id feature. Can't add it to the registry");
+		if (!map.containsKey(id)) {
+			URI uri = bo.eResource().getURI();
+			map.put(id, uri.toPlatformString(true));
+		} else {
+			System.out.println(String.format("Found element for id %s. Nothing to do...", id));
+		}
+	}
+	
+	public void addElement(String key, String uri) {
+		map.put(key, uri);
+	}
+	
+	public String generateURI(EObject bo) {
+		Resource res = bo.eResource();
+		String fragment = res.getURIFragment(bo);
+		URI baseURI = res.getURI();
+		URI full = baseURI.appendFragment(fragment);
+		
+		return full.toString();
+	}
+	
+	public EObject getEObject(String key) {
+		EObject bo = null;
+		String uri = map.get(key);
+		if (uri != null) {
+			URI full = URI.createURI(uri, true);
+			Resource res = new ResourceSetImpl().getResource(full, true);
+			if (full.fragment() != null) {
+				bo = res.getEObject(full.fragment());
+			}			
+		}
+		return bo;
+	}
+	
+	public boolean load() {
+		File file = getFile();
+		if (file != null && file.exists()) {
+			try {
+				FileInputStream fis = new FileInputStream(file);
+				Properties props = new Properties();
+				props.loadFromXML(fis);
+				for (Entry<Object, Object> e : props.entrySet()) {
+					map.put((String) e.getKey(), (String)e.getValue());
+				}
+				fis.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+	
+	public boolean save() {
+			try {
+				File file = getFile();
+				if (!file.exists()) {
+					file.createNewFile();
+				}
+				FileOutputStream fos = new FileOutputStream(file);
+				Properties props = new Properties();
+				props.putAll(map);
+				props.storeToXML(fos, "The reference registry");
+				fos.close();
+				return true;
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		return false;
+	}
+	
+	public void clearRegistry() {
+		this.map = new HashMap<String, String>();
+		this.count = 0;
+	}
+	
+	public void print() {
+		System.out.println("Current count value: " + count);
+		System.out.println("RefReg map contents:.....");
+		for (Entry<String, String> e : map.entrySet()) {
+			System.out.println(String.format("KEY: %d \t VALUE: %s", e.getKey(), e.getValue()));
+		}
+		System.out.println("");
+	}
+	
+	private File getFile() {
+		IPath base = ResourcesPlugin.getWorkspace().getRoot().getLocation();
+		IPath path = base.append(new Path(REF_REG_FILE));
+		File file = path.toFile();
+		return file;
+	}
+
+}
+*/
