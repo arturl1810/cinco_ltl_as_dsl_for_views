9f617945d99391d56047790f19dc43ee2058b3c8
Dawid Kopetzki <dawid.kopetzki@tu-dortmund.de>
2015-12-10 12:55:02 +0100

    Bugfix: Deletion of a node triggers the preDelete hook of the attached edges

--- de.jabc.cinco.meta.core.ui/src/de/jabc/cinco/meta/core/ui/features/CincoDeleteFeature.java
+++ de.jabc.cinco.meta.core.ui/src/de/jabc/cinco/meta/core/ui/features/CincoDeleteFeature.java
@@ -3,12 +3,20 @@ package de.jabc.cinco.meta.core.ui.features;
 import graphmodel.Edge;
 import graphmodel.GraphModel;
 import graphmodel.ModelElementContainer;
+import graphmodel.Node;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.eclipse.graphiti.features.IDeleteFeature;
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.context.IDeleteContext;
+import org.eclipse.graphiti.features.context.impl.DeleteContext;
+import org.eclipse.graphiti.features.context.impl.RemoveContext;
+import org.eclipse.graphiti.features.impl.DefaultRemoveFeature;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.ui.features.DefaultDeleteFeature;
 
 public class CincoDeleteFeature extends DefaultDeleteFeature {
@@ -30,7 +38,28 @@ public class CincoDeleteFeature extends DefaultDeleteFeature {
 
 	@Override
 	public void delete(IDeleteContext context) {
+		Object bo = getBusinessObjectForPictogramElement(context.getPictogramElement());
+		if (bo instanceof Node) {
+			List<Edge> all = new ArrayList<Edge>();
+			all.addAll(((Node) bo).getIncoming());
+			all.addAll(((Node) bo).getOutgoing());
+			for (Edge e : all) {
+				PictogramElement pe = Graphiti.getLinkService().getPictogramElements(getDiagram(), e).get(0);
+				DeleteContext dc = new DeleteContext(pe);
+				IDeleteFeature df = getFeatureProvider().getDeleteFeature(dc);
+				if (df.canDelete(dc))
+					df.delete(dc);
+				RemoveContext rc = new RemoveContext(pe);
+				DefaultRemoveFeature drf = new DefaultRemoveFeature(getFeatureProvider());
+				if (drf.canRemove(rc))
+					drf.remove(rc);
+			}
+		}
 		super.delete(context);
+		RemoveContext rc = new RemoveContext(context.getPictogramElement());
+		DefaultRemoveFeature drf = new DefaultRemoveFeature(getFeatureProvider());
+		if (drf.canRemove(rc))
+			drf.remove(rc);
 	}
 	
 	private void deleteDanglingEdges(ModelElementContainer mec) {
