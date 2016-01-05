7a2cc8820cbba1eb6eb4c3393440f1b1a460f158
Michael Lybecait <michael.lybecait@tu-dortmund.de>
2016-01-05 14:25:40 +0100

    removed runnable with progress

--- de.jabc.cinco.meta.core/src/de/jabc/cinco/meta/core/feature/handler/GenerateFeatureProjectHandler.java
+++ de.jabc.cinco.meta.core/src/de/jabc/cinco/meta/core/feature/handler/GenerateFeatureProjectHandler.java
@@ -1,10 +1,8 @@
 package de.jabc.cinco.meta.core.feature.handler;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-import mgl.Annotation;
 import mgl.GraphModel;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -14,13 +12,11 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.progress.IProgressService;
 import org.eclipse.xtext.resource.IResourceDescriptions;
@@ -28,10 +24,10 @@ import org.eclipse.xtext.ui.resource.IResourceSetProvider;
 
 import com.google.inject.Inject;
 
+import de.jabc.cinco.meta.core.BundleRegistry;
 import de.jabc.cinco.meta.core.featuregenerator.Generate_Feature_XML;
 import de.jabc.cinco.meta.core.pluginregistry.PluginRegistry;
 import de.jabc.cinco.meta.core.ui.listener.MGLSelectionListener;
-import de.jabc.cinco.meta.core.BundleRegistry;
 import de.jabc.cinco.meta.core.utils.projects.ProjectCreator;
 import de.metaframe.jabc.framework.execution.DefaultLightweightExecutionEnvironment;
 import de.metaframe.jabc.framework.execution.LightweightExecutionEnvironment;
@@ -55,94 +51,66 @@ public class GenerateFeatureProjectHandler extends AbstractHandler {
 			IFile file = MGLSelectionListener.INSTANCE.getCurrentMGLFile();
 			if(file!=null){
 				ResourceSet rSet = new ResourceSetImpl();
-				ps.run(true,false,new FeatureGenerator(file,rSet));
+				generateFeature(file,rSet);
 				
 			}
-		} catch (InvocationTargetException | InterruptedException e) {
-			e.printStackTrace();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
-		return false;
+		return null;
 	}
 
-	private class FeatureGenerator implements IRunnableWithProgress {
+	private void generateFeature(IFile file, ResourceSet rSet) {
+		Resource res = rSet.createResource(URI
+				.createPlatformResourceURI(file.getFullPath()
+						.toPortableString(), true));
+		try {
+			res.load(null);
+			GraphModel model = (GraphModel) res.getContents()
+					.get(0);
+			String packageName = ProjectCreator.getProjectSymbolicName(ProjectCreator.getProject(model.eResource())); 
+			String projectName = packageName + ".feature";
+			ArrayList<String> srcFolders = new ArrayList<>();
+			srcFolders.add("src");
+			ArrayList<IProject> referencedProjects = new ArrayList<>();
+			HashSet<String> requiredBundles = new HashSet<>();
+			IResource iRes = ResourcesPlugin.getWorkspace().getRoot().findMember(new Path(model.eResource().getURI().toPlatformString(true)));
+			String symbolicName;
+			if (iRes != null)
+				symbolicName = ProjectCreator.getProjectSymbolicName(iRes.getProject());
+			else symbolicName = model.getPackage();
+			requiredBundles.add(symbolicName);
+			ArrayList<String> exportedPackages = new ArrayList<>();
+			ArrayList<String> additionalNatures = new ArrayList<>();
+			additionalNatures.add("org.eclipse.pde.FeatureNature");
 
-		private ExecutionEvent event;
-		private IFile file;
-		private ResourceSet rSet;
-
-		public FeatureGenerator(IFile file, ResourceSet rSet) {
-			this.file = file;
-			this.rSet = rSet;
+			IProject featureProject = ProjectCreator.createProject(
+					projectName, srcFolders, referencedProjects,
+					requiredBundles, exportedPackages,
+					additionalNatures, null,false);
+			String featureProjectPath = featureProject.getLocation().addTrailingSeparator().toPortableString();
+			
+			// Generating feature.xml
+			LightweightExecutionContext context = new DefaultLightweightExecutionContext(null);
+			context.put("packageName", packageName);
+			context.put("annotationToMGLPackageMap", PluginRegistry.getInstance().getMGLDependentPlugins());
+			context.put("annotationToPackageMap", PluginRegistry.getInstance().getUsedPlugins());
+			context.put("annotationToFragmentMap", PluginRegistry.getInstance().getUsedFragments());
+			context.put("annotationToMGLFragmentMap", PluginRegistry.getInstance().getMGLDependentFragments());
+			context.put("annotationToPackageMap", PluginRegistry.getInstance().getUsedPlugins());
+			context.put("featureProjectPath",featureProjectPath);
+			context.put("graphModel",model);
+			context.put("otherPluginIDs", BundleRegistry.INSTANCE.getPluginIDs());
+			context.put("otherFragmentIDs", BundleRegistry.INSTANCE.getFragmentIDs());
+			LightweightExecutionEnvironment env = new DefaultLightweightExecutionEnvironment(context);
+			if(new Generate_Feature_XML().execute(env).equals("error"))
+				throw new Exception("Could not create feature.xml",(Throwable)context.get("exception"));
+			featureProject.refreshLocal(IResource.DEPTH_INFINITE,
+					null);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
-
-		@Override
-		public void run(IProgressMonitor monitor)
-				throws InvocationTargetException, InterruptedException {
-
-					Resource res = rSet.createResource(URI
-							.createPlatformResourceURI(file.getFullPath()
-									.toPortableString(), true));
-					try {
-						res.load(null);
-						GraphModel model = (GraphModel) res.getContents()
-								.get(0);
-						String packageName = ProjectCreator.getProjectSymbolicName(ProjectCreator.getProject(model.eResource())); 
-						String projectName = packageName + ".feature";
-						ArrayList<String> srcFolders = new ArrayList<>();
-						srcFolders.add("src");
-						ArrayList<IProject> referencedProjects = new ArrayList<>();
-						HashSet<String> requiredBundles = new HashSet<>();
-						IResource iRes = ResourcesPlugin.getWorkspace().getRoot().findMember(new Path(model.eResource().getURI().toPlatformString(true)));
-						String symbolicName;
-						if (iRes != null)
-							symbolicName = ProjectCreator.getProjectSymbolicName(iRes.getProject());
-						else symbolicName = model.getPackage();
-						requiredBundles.add(symbolicName);
-						/**
-						 * Since the graphiti code is generated into the main project, this dependency is 
-						 * not necessary anymore.
-						 */
-//						if (hasStyleAnnotation(model))
-//							requiredBundles.add(packageName + ".graphiti");
-						ArrayList<String> exportedPackages = new ArrayList<>();
-						ArrayList<String> additionalNatures = new ArrayList<>();
-						additionalNatures.add("org.eclipse.pde.FeatureNature");
-
-						IProject featureProject = ProjectCreator.createProject(
-								projectName, srcFolders, referencedProjects,
-								requiredBundles, exportedPackages,
-								additionalNatures, monitor,false);
-						String featureProjectPath = featureProject.getLocation().addTrailingSeparator().toPortableString();
-						
-						// Generating feature.xml
-						LightweightExecutionContext context = new DefaultLightweightExecutionContext(null);
-						context.put("packageName", packageName);
-						context.put("annotationToMGLPackageMap", PluginRegistry.getInstance().getMGLDependentPlugins());
-						context.put("annotationToPackageMap", PluginRegistry.getInstance().getUsedPlugins());
-						context.put("annotationToFragmentMap", PluginRegistry.getInstance().getUsedFragments());
-						context.put("annotationToMGLFragmentMap", PluginRegistry.getInstance().getMGLDependentFragments());
-						context.put("annotationToPackageMap", PluginRegistry.getInstance().getUsedPlugins());
-						context.put("featureProjectPath",featureProjectPath);
-						context.put("graphModel",model);
-						context.put("otherPluginIDs", BundleRegistry.INSTANCE.getPluginIDs());
-						context.put("otherFragmentIDs", BundleRegistry.INSTANCE.getFragmentIDs());
-						LightweightExecutionEnvironment env = new DefaultLightweightExecutionEnvironment(context);
-						if(new Generate_Feature_XML().execute(env).equals("error"))
-							throw new Exception("Could not create feature.xml",(Throwable)context.get("exception"));
-						featureProject.refreshLocal(IResource.DEPTH_INFINITE,
-								monitor);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				
-			}
-	}
-	
-	private boolean hasStyleAnnotation(GraphModel model) {
-		for (Annotation a : model.getAnnotations()) {
-			if ("style".equals(a.getName()))
-				return true;
-		}
-		return false;
+		
 	}
 }
+
