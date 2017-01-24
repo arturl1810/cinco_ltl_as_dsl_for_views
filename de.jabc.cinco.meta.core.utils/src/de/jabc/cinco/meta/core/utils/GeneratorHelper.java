be653d61f7313d117de8f98eebb09c0cecaf6537
Stefan Naujokat <stefan.naujokat@tu-dortmund.de>
2017-01-24 11:01:39 +0100

    refactoring code generation: removed duplicate code

--- de.jabc.cinco.meta.core.utils/src/de/jabc/cinco/meta/core/utils/GeneratorHelper.java
+++ de.jabc.cinco.meta.core.utils/src/de/jabc/cinco/meta/core/utils/GeneratorHelper.java
@@ -6,14 +6,12 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.emf.codegen.ecore.generator.Generator;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
-import org.eclipse.emf.codegen.ecore.genmodel.GenModelPackage;
 import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
 import org.eclipse.emf.codegen.ecore.genmodel.generator.GenBaseGeneratorAdapter;
 import org.eclipse.emf.common.util.BasicMonitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
 public class GeneratorHelper {
@@ -45,35 +43,21 @@ public class GeneratorHelper {
 		}
 	}
 	
+	/**
+	 * convenience method for {@link #generateGenModelCode(IProject, String)} that extracts
+	 * the project and model name from the provided MGL file
+	 * 
+	 * @param mglModelFile
+	 * @throws IOException
+	 */
 	public static void generateGenModelCode(IFile mglModelFile) throws IOException {
 		String modelName = (mglModelFile.getName().endsWith(".mgl")) 
 				? mglModelFile.getName().split("\\.")[0] 
 				: mglModelFile.getName();
 				
 		IProject project = mglModelFile.getProject();
-		IFile genModelFile = project.getFile("src-gen/model/" + modelName +".genmodel");
 		
-		if (!genModelFile.exists())
-			throw new IOException("The file: " + modelName+".genmodel does not exist");
-		
-		Resource res = new ResourceSetImpl().getResource(
-				URI.createPlatformResourceURI(genModelFile.getFullPath().toOSString(), true),true);
-		res.load(null);
-		for (EObject o : res.getContents()) {	
-			if (o instanceof GenModel) {
-				GenModel genModel = (GenModel) o;
-				for (GenPackage gm : genModel.getUsedGenPackages()) {
-					if (!gm.getGenModel().equals(genModel)) {
-						genModel.getUsedGenPackages().add(gm);
-					}
-				}
-				//System.out.println(genModel.getUsedGenPackages());
-				genModel.setCanGenerate(true);
-				genModel.reconcile();
-				Generator generator = new Generator();
-				generator.setInput(genModel);
-				generator.generate(genModel, GenBaseGeneratorAdapter.MODEL_PROJECT_TYPE, new BasicMonitor());
-			}
-		}
+		generateGenModelCode(project, modelName);
 	}
+		
 }
